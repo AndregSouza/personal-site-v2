7495127f5ca4b2811348281986e85e9336ebde21
AndregSouza <andregsouzaweb@gmail.com>
2024-09-05 15:25:07 -0300

    Second Version of Portfolio

---
+++ js/stylechange.js
@@ -0,0 +1,119 @@
+$(function() {
+  $('#lumini').hover(function() {
+    $('#image-work').css('background-image', 'url(./images/lumini.jpg)');
+    $('#entrelinhas').css('opacity', '0.3');
+    $('#google').css('opacity', '0.3');
+    $('#uber').css('opacity', '0.3');
+    $('#paypal').css('opacity', '0.3');
+    $('#livelo').css('opacity', '0.3');
+  },
+  function() {
+    // on mouseout, reset the background colour
+    $('#lumini').css('opacity', '1');
+    $('#entrelinhas').css('opacity', '1');
+    $('#google').css('opacity', '1');
+    $('#uber').css('opacity', '1');
+    $('#paypal').css('opacity', '1');
+    $('#livelo').css('opacity', '1');
+  });
+});
+
+$(function() {
+  $('#entrelinhas').hover(function() {
+    $('#image-work').css('background-image', 'url(./images/entrelinhas.jpg)');
+    $('#lumini').css('opacity', '0.3');
+    $('#google').css('opacity', '0.3');
+    $('#uber').css('opacity', '0.3');
+    $('#paypal').css('opacity', '0.3');
+    $('#livelo').css('opacity', '0.3');
+  },
+  function() {
+    // on mouseout, reset the background colour
+    $('#lumini').css('opacity', '1');
+    $('#entrelinhas').css('opacity', '1');
+    $('#google').css('opacity', '1');
+    $('#uber').css('opacity', '1');
+    $('#paypal').css('opacity', '1');
+    $('#livelo').css('opacity', '1');
+  });
+});
+
+$(function() {
+  $('#google').hover(function() {
+    $('#image-work').css('background-image', 'url(./images/google-play.jpg)');
+    $('#lumini').css('opacity', '0.3');
+    $('#entrelinhas').css('opacity', '0.3');
+    $('#uber').css('opacity', '0.3');
+    $('#paypal').css('opacity', '0.3');
+    $('#livelo').css('opacity', '0.3');
+  },
+  function() {
+    // on mouseout, reset the background colour
+    $('#lumini').css('opacity', '1');
+    $('#entrelinhas').css('opacity', '1');
+    $('#google').css('opacity', '1');
+    $('#uber').css('opacity', '1');
+    $('#paypal').css('opacity', '1');
+    $('#livelo').css('opacity', '1');
+  });
+});
+
+$(function() {
+  $('#uber').hover(function() {
+    $('#image-work').css('background-image', 'url(./images/uber.jpg)');
+    $('#lumini').css('opacity', '0.3');
+    $('#entrelinhas').css('opacity', '0.3');
+    $('#google').css('opacity', '0.3');
+    $('#paypal').css('opacity', '0.3');
+    $('#livelo').css('opacity', '0.3');
+  },
+  function() {
+    // on mouseout, reset the background colour
+    $('#lumini').css('opacity', '1');
+    $('#entrelinhas').css('opacity', '1');
+    $('#google').css('opacity', '1');
+    $('#uber').css('opacity', '1');
+    $('#paypal').css('opacity', '1');
+    $('#livelo').css('opacity', '1');
+  });
+});
+
+$(function() {
+  $('#paypal').hover(function() {
+    $('#image-work').css('background-image', 'url(./images/entrelinhas2.jpg)');
+    $('#lumini').css('opacity', '0.3');
+    $('#entrelinhas').css('opacity', '0.3');
+    $('#google').css('opacity', '0.3');
+    $('#uber').css('opacity', '0.3');
+    $('#livelo').css('opacity', '0.3');
+  },
+  function() {
+    // on mouseout, reset the background colour
+    $('#lumini').css('opacity', '1');
+    $('#entrelinhas').css('opacity', '1');
+    $('#google').css('opacity', '1');
+    $('#uber').css('opacity', '1');
+    $('#paypal').css('opacity', '1');
+    $('#livelo').css('opacity', '1');
+  });
+});
+
+$(function() {
+  $('#livelo').hover(function() {
+    $('#image-work').css('background-image', 'url(./images/livelo.jpg)');
+    $('#lumini').css('opacity', '0.3');
+    $('#entrelinhas').css('opacity', '0.3');
+    $('#google').css('opacity', '0.3');
+    $('#uber').css('opacity', '0.3');
+    $('#paypal').css('opacity', '0.3');
+  },
+  function() {
+    // on mouseout, reset the background colour
+    $('#lumini').css('opacity', '1');
+    $('#entrelinhas').css('opacity', '1');
+    $('#google').css('opacity', '1');
+    $('#uber').css('opacity', '1');
+    $('#paypal').css('opacity', '1');
+    $('#livelo').css('opacity', '1');
+  });
+});
