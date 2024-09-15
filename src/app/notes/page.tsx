968d036dc8128c65debc55f1adaa2caefc43c2f9
AndregSouza <andregsouzaweb@gmail.com>
2024-09-15 11:53:05 -0300

    Fixing button acessibility

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -20,40 +20,36 @@ export default function Notes() {
 			<h1> Notes </h1>
 			<ul className="flex flex-1 flex-col gap-2">
 				<li>
-					<Link className="w-full justify-between p-2 -mx-2" href="/notes/redesigning-my-portfolio">
-						<Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-							<h3>Redesigning my portfolio</h3>
-							<Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
+					<Button asChild variant="ghost">
+						<Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/redesigning-my-portfolio"> Redesigning my portfolio
+							<Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
 							<span className="tabular text-sm font-normal text-muted-foreground self-center">Sep 2024</span>
-						</Button>
-					</Link>
+						</Link>
+					</Button>
 				</li>
 				<li>
-					<Link className="w-full justify-between p-2 -mx-2" href="/notes/the-zettelkasten-method">
-						<Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-							<h3>The Zettelkasten method</h3>
-							<Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
+					<Button asChild variant="ghost">
+						<Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/the-zettelkasten-method"> The Zettelkasten method
+							<Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
 							<span className="tabular text-sm font-normal text-muted-foreground self-center">Feb 2023</span>
-						</Button>
-					</Link>
+						</Link>
+					</Button>
 				</li>
 				<li>
-					<Link className="w-full justify-between p-2 -mx-2" href="/notes/how-to-study-&-learn">
-						<Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-							<h3>How to study & learn</h3>
-							<Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
-							<span className="tabular text-sm font-normal text-muted-foreground self-center">Feb 2023</span>
-						</Button>
-					</Link>
-				</li>
-				<li>
-					<Link className="w-full justify-between p-2 -mx-2" href="/notes/conversational-interfaces">
-						<Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-							<h3>Conversational interfaces</h3>
-							<Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
+					<Button asChild variant="ghost">
+						<Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/how-to-study-&-learn"> How to study & learn
+							<Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
 							<span className="tabular text-sm font-normal text-muted-foreground self-center">Dec 2018</span>
-						</Button>
-					</Link>
+						</Link>
+					</Button>
+				</li>
+				<li>
+					<Button asChild variant="ghost">
+						<Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/conversational-interfaces"> Conversational interfaces
+							<Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
+							<span className="tabular text-sm font-normal text-muted-foreground self-center">Dec 2018</span>
+						</Link>
+					</Button>
 				</li>
 			</ul>
 		</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,9 +39,9 @@ export default function Home() {
       <section className="flex flex-1 flex-col gap-6">
         <div className="flex justify-between place-items-center">
           <h2> Projects </h2>
-          <Link href="/projects">
-            <Button variant="ghost" size="sm"> View all </Button>
-          </Link>
+          <Button asChild variant="ghost" size="sm">
+            <Link href="/projects"> View all </Link>
+          </Button>
         </div>
 
         <div className="grid sm:grid-cols-3 gap-4">
@@ -80,56 +80,53 @@ export default function Home() {
             </Card>
           </Link>
         </div>
-      </section>
+      </section >
 
       <Separator></Separator>
 
       <section className="flex flex-1 flex-col gap-6">
         <div className="flex justify-between place-items-center">
           <h2> Notes </h2>
-          <Link href="/notes">
-            <Button variant="ghost" size="sm"> View all </Button>
-          </Link>
+          <Button asChild variant="ghost" size="sm">
+            <Link href="/notes"> View all </Link>
+          </Button>
+
         </div>
         <ul className="flex flex-1 flex-col gap-2">
           <li>
-            <Link className="w-full justify-between p-2 -mx-2" href="/notes/redesigning-my-portfolio">
-              <Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-                <h3>Redesigning my portfolio</h3>
-                <Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
+            <Button asChild variant="ghost">
+              <Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/redesigning-my-portfolio"> Redesigning my portfolio
+                <Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
                 <span className="tabular text-sm font-normal text-muted-foreground self-center">Sep 2024</span>
-              </Button>
-            </Link>
+              </Link>
+            </Button>
           </li>
           <li>
-            <Link className="w-full justify-between p-2 -mx-2" href="/notes/the-zettelkasten-method">
-              <Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-                <h3>The Zettelkasten method</h3>
-                <Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
+            <Button asChild variant="ghost">
+              <Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/the-zettelkasten-method"> The Zettelkasten method
+                <Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
                 <span className="tabular text-sm font-normal text-muted-foreground self-center">Feb 2023</span>
-              </Button>
-            </Link>
+              </Link>
+            </Button>
           </li>
           <li>
-            <Link className="w-full justify-between p-2 -mx-2" href="/notes/how-to-study-&-learn">
-              <Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-                <h3>How to study & learn</h3>
-                <Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
-                <span className="tabular text-sm font-normal text-muted-foreground self-center">Feb 2023</span>
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link className="w-full justify-between p-2 -mx-2" href="/notes/conversational-interfaces">
-              <Button variant="ghost" size="lg" className="w-[calc(100%+32px)] justify-between px-4 -mx-4">
-                <h3>Conversational interfaces</h3>
-                <Separator className="mx-4 flex grow shrink w-full mix-blend-multiply dark:mix-blend-lighten"></Separator>
+            <Button asChild variant="ghost">
+              <Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/how-to-study-&-learn"> How to study & learn
+                <Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
                 <span className="tabular text-sm font-normal text-muted-foreground self-center">Dec 2018</span>
-              </Button>
-            </Link>
+              </Link>
+            </Button>
+          </li>
+          <li>
+            <Button asChild variant="ghost">
+              <Link className="w-[calc(100%+32px)] justify-between -mx-4" href="/notes/conversational-interfaces"> Conversational interfaces
+                <Separator className="mx-4 flex grow shrink mix-blend-multiply dark:mix-blend-lighten"></Separator>
+                <span className="tabular text-sm font-normal text-muted-foreground self-center">Dec 2018</span>
+              </Link>
+            </Button>
           </li>
         </ul>
       </section>
-    </div>
+    </div >
   );
 }
